bd683fa287c7e850ed3b2b8da83dfa67e8383d3f
robynsm <robynamysmith92@gmail.com>
2022-08-25 21:55:09 +0100

    Added guest user for users to see full functionality of site

--- models/user_data.py
+++ models/user_data.py
@@ -2,5 +2,6 @@ from models.user import UserModel
 
 user_list = [
     UserModel(username="Robyn", password="Boogalu23", email="robynamysmith92@gmail.com"),
-    UserModel(username="Stu", password="Tangerine23", email="stu@gmail.com")
+    UserModel(username="Stu", password="Tangerine23", email="stu@gmail.com"),
+    UserModel(username="Guest", password="Welcome", email="guest@gmail.com")
 ]
