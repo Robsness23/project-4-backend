92e94a5b11297f5b64ecb5778beaf1c9d26acb6f
robynsm <robynamysmith92@gmail.com>
2022-08-25 22:05:03 +0100

    Changed email for guest

--- models/user_data.py
+++ models/user_data.py
@@ -3,5 +3,5 @@ from models.user import UserModel
 user_list = [
     UserModel(username="Robyn", password="Boogalu23", email="robynamysmith92@gmail.com"),
     UserModel(username="Stu", password="Tangerine23", email="stu@gmail.com"),
-    UserModel(username="Guest", password="Welcome", email="guest@gmail.com")
+    UserModel(username="Guest", password="Welcome", email="guest@pollinator.com")
 ]
